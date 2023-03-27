bae652ce698b916b39f71857c516f09570b61ecd
Damir <38811598+MrDemSay@users.noreply.github.com>
2023-03-27 20:12:05 +0400

    Add files via upload

---
+++ smart_pointer.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+
+template <class Type>
+
+class smart_ptr {
+	private:
+		Type* pointer;
+	public:
+		smart_ptr(Type* p): pointer(p) {};
+		operator Type*() { return pointer; };
+		Type *operator -> () {
+			if (!pointer) {
+				pointer = new Type();
+				std::cerr << "Bad pointer..." << std::endl;
+			} 
+			return pointer; 
+		};	
+};
+
+class F {
+	private:
+		int a, b;
+	public:
+		F(): a(0), b(0) {};
+		F (int a, int b): a(a), b(b) {};
+		int Sum() { return a + b; }
+};
+
+int main(int argc, char const *argv[])
+{
+	smart_ptr<F> sp(NULL);
+	std::cout << sp -> Sum() << std::endl;
+	return 0;
+}
+
+/*Бывают случаи когда укзаатель указывает на NULL и тогда при разименовании указателя возникает ошибка, умные указатели могут помочь решить данную проблему
+	
+*/
